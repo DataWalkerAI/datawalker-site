19b87ec58dc4e9886ba03fac9682f9c4978a9525
DataWalker <datawalker@proton.me>
2026-01-19 00:05:51 +0100

    modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
 plugins: [react()],
-//base: '/datawalker-site/',
+base: '/datawalker-site/',
 })
 // Наприклад, якщо репозиторій називається datawalker-site:
 // base: '/datawalker-site/'
