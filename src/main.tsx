c45fd17df1b06c0a33c2e0d989d3ef37bad577ce
DataWalker <datawalker@proton.me>
2026-01-18 22:52:10 +0100

    modified:   src/main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css'; // Базові стилі
-import typescriptLogo from './typescript.svg';
-import { setupCounter } from './counter';
+// import typescriptLogo from './typescript.svg';
+// import { setupCounter } from './counter';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
