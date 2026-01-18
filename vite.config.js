3e45940b87b74c2f3bfd792c3a6e4bacf18501ec
DataWalker <datawalker@proton.me>
2026-01-18 22:10:51 +0100

    modified:   package-lock.json 	modified:   package.json 	new file:   vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+base: '/datawalker-site/', // <-- заміни на точну назву свого GitHub repo
+})
+// Наприклад, якщо репозиторій називається datawalker-site:
+// base: '/datawalker-site/'
